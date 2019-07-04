04bc86d899fca943223f712ff0020bda0a233514
jk <kerntzj@gmail.com>
2019-07-04 10:54:56 -0700

    add support for images in app with url/file loaders

--- webpack.config.js
+++ webpack.config.js
@@ -45,7 +45,17 @@ module.exports = {
                         
                     ]
                 }
-            }
+            },
+            {
+                test: /\.(png|gif|jp(e*)g|svg)$/,
+                use: {
+                  loader: 'url-loader',
+                  options: {
+                    limit: 8000,
+                    name: 'images/[hash]-[name].[ext]'
+                  }
+                }
+              }
         ]
     },
 
