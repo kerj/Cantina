c5ea0897069866e4db7059147771cf536948c1ef
jk <kerntzj@gmail.com>
2019-07-04 10:04:14 -0700

    fight with the linter, and add client routing

---
+++ src/components/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Switch, Route } from 'react-router-dom';
+import Header from './Header';
+import KegList from './KegList';
+import NewKegForm from './NewKegForm';
+
+const styles = makeStyles({
+  header: {
+    color: 'red'
+  }
+})
+
+function App() {
+  const classes = styles();
+  return (
+    <div>
+      <Header/>
+      <Switch>
+        <Route exact path='/' component={KegList}/>
+        <Route path='/newKeg' component={NewKegForm}/>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
