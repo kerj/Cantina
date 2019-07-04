2e659512837c7d6af1913f547a3650d53dd7eb6c
jk <kerntzj@gmail.com>
2019-07-04 10:21:19 -0700

    Add hardcoded data to display for drinks

--- src/components/KegList.jsx
+++ src/components/KegList.jsx
@@ -1,5 +1,27 @@
 import React from 'react'
 
+const masterKegList = [
+    {
+        drinkType: 'Alcoholic',
+        drink: [
+            'Jawa Juice',
+            'Naboo Bug Juice',
+            'Tatooine Bug Juice',
+            'Juri Juice'
+        ]
+    },
+    {
+        drinkType: 'Non-Alcohiolic',
+        drink: [
+            'Zoochberry Juice',
+            'Spacer Juice',
+            'Jaffa Cider'
+        ]
+    }
+      
+
+]
+
 export default function KegList() {
     return (
         <div>
